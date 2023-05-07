cbc61946d3a270eb2c56f9b42039b9636055d680
Kanghee Han <hcsy1219@gmail.com>
2023-05-07 15:28:29 +0900

    Recover lost data due to branch change

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Function function = new Function();
+        function.NonPreemptivePriority();
+    }
+}
